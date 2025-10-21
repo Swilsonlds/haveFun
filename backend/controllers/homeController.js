420c626dbab6344663252dfec32030c24c1ca52e
Spencer Wilson <swilsonlds@gmail.com>
2025-10-21 14:58:32 -0600

    added test model to ensure connection to mongodb works

--- backend/controllers/homeController.js
+++ backend/controllers/homeController.js
@@ -1,3 +1,10 @@
-exports.getHome = (req, res) => {
-  res.send('<h1>Hello from your Express Server (MVC Style)!</h1>');
+const Test = require('../models/Test');
+
+exports.getHome = async (req, res) => {
+  try {
+    const doc = await Test.create({ name: 'MongoDB Works!' });
+    res.send(`<h1>${doc.name}</h1>`);
+  } catch (err) {
+    res.status(500).send('MongoDB error: ' + err.message);
+  }
 };
